9e07739db295f24b128752f8a00ff4f6ee1fc3e3
yaswanth8 <ykondaveeti@gmail.com>
2024-04-18 16:38:36 -0500

    added property files

--- src/main/java/com/careerit/cbook/CbookAppApplication.java
+++ src/main/java/com/careerit/cbook/CbookAppApplication.java
@@ -1,13 +1,27 @@
 package com.careerit.cbook;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CbookAppApplication {
 
+	@Value("${greetings}")
+	private String greetings;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CbookAppApplication.class, args);
+	}
+
+
+	@Bean
+	public CommandLineRunner commandLineRunner(){
+		return args -> {
+			System.out.println("Hello, "+greetings);
+		};
 	}
 
 }
