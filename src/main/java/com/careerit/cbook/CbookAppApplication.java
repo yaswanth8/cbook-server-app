abf8691edbfa09132266156c32d69bbe0fdab38f
yaswanth8 <ykondaveeti@gmail.com>
2024-05-08 18:24:55 -0500

    Enable scheduler

--- src/main/java/com/careerit/cbook/CbookAppApplication.java
+++ src/main/java/com/careerit/cbook/CbookAppApplication.java
@@ -22,6 +22,7 @@ public class CbookAppApplication {
 	private EmailService emailService;
 
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(CbookAppApplication.class, args);
 	}
